1a50e0feebddefe3a12952fe5d19f2ae33acbdd9
danieljkpraveen <danieljkpraveen@gmail.com>
2023-08-08 18:31:53 +0530

    minor changes

--- diskvr/settings.py
+++ diskvr/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.getenv('STATIC_URL')
-MEDIA_URL = os.getenv('MEDIA_URL')
-MEDIA_ROOT = os.getenv('MEDIA_ROOT')
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
