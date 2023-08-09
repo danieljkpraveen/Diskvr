a609a37d1afbd1e65ccf8451917fcea63d93a831
danieljkpraveen <danieljkpraveen@gmail.com>
2023-08-09 15:24:42 +0530

    updated settings

--- diskvr/settings.py
+++ diskvr/settings.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-dq%489eg8=-$hwyycimpn98(9s93mju2gq$(qtfxf!j^lf5fes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
