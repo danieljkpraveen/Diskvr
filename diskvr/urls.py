7e29dcb3ba0bb6fcdac974511187c9600c0177ac
danieljkpraveen <danieljkpraveen@gmail.com>
2023-08-09 13:37:51 +0530

    updated url

--- diskvr/urls.py
+++ diskvr/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('core.urls')),
     path('items/', include('inventory.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
