eda7bfd4a16b19d2df1fd67a9e80b5d8d3880443
danieljkpraveen <danieljkpraveen@gmail.com>
2023-08-09 13:25:44 +0530

    changed debug status

--- diskvr/settings.py
+++ diskvr/settings.py
@@ -16,10 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dq%489eg8=-$hwyycimpn98(9s93mju2gq$(qtfxf!j^lf5fes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['danieljkpraveen.pythonanywhere.com',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'danieljkpraveen.pythonanywhere.com',]
 
 
 # Application definition
@@ -69,23 +69,23 @@ WSGI_APPLICATION = 'diskvr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'neondb',
-    'USER': 'danieljkpraveen',
-    'PASSWORD': '6MxuTpjsI2BE',
-    'HOST': 'ep-curly-poetry-75866079.us-east-2.aws.neon.tech',
-    'PORT': '5432',
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'neondb',
+#     'USER': 'danieljkpraveen',
+#     'PASSWORD': '6MxuTpjsI2BE',
+#     'HOST': 'ep-curly-poetry-75866079.us-east-2.aws.neon.tech',
+#     'PORT': '5432',
+#   }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
