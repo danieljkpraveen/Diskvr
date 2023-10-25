a0520917b13b645cd3de96aeac5d43d1a755a2f0
danieljkpraveen <danieljkpraveen@gmail.com>
2023-10-25 21:52:55 +0530

    updated order model to have status field

--- inventory/models.py
+++ inventory/models.py
@@ -4,14 +4,36 @@ from django.contrib.auth.models import User
 
 
 class NeonLights(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    image = models.ImageField(upload_to='inventory_images', blank=True, null=True)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    image = models.ImageField(
+        upload_to='inventory_images',
+        blank=True,
+        null=True
+    )
     name = models.CharField(max_length=255)
-    price = models.FloatField(blank=True, null=True)
+    price = models.FloatField(
+        blank=True,
+        null=True
+    )
     inches = models.CharField(max_length=255)
-    ip_rating = models.CharField(max_length=150, blank=True, null=True)
-    led_lights_used = models.IntegerField(blank=True, null=True)
-    sheet_mm = models.CharField(max_length=100, blank=True, null=True)
+    ip_rating = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True
+    )
+    led_lights_used = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    sheet_mm = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
@@ -29,14 +51,44 @@ class NeonLights(models.Model):
 
 
 class Order(models.Model):
-    order_id = models.CharField(max_length=36, primary_key=True, editable=False)
-    product_name = models.CharField(max_length=255, editable=False)
-    price = models.FloatField(null=True, blank=True)
-    product_image_path = models.CharField(max_length=255, editable=False)
-    username = models.CharField(max_length=150, editable=False)
+
+    STATUS_CHOICES = [
+        ('NA', 'Not accepted'),
+        ('IP', 'In progress'),
+        ('CO', 'Complete'),
+    ]
+
+    order_id = models.CharField(
+        max_length=36,
+        primary_key=True,
+        editable=False
+    )
+    product_name = models.CharField(
+        max_length=255,
+        editable=False
+    )
+    price = models.FloatField(
+        null=True,
+        blank=True
+    )
+    product_image_path = models.CharField(
+        max_length=255,
+        editable=False
+    )
+    username = models.CharField(
+        max_length=150,
+        editable=False
+    )
     email = models.EmailField(editable=False)
-    phone_number = models.CharField(max_length=15, editable=False)
-    complete = models.BooleanField(default=False)
+    phone_number = models.CharField(
+        max_length=15,
+        editable=False
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default='NA',
+    )
 
     def __str__(self):
         return self.order_id
